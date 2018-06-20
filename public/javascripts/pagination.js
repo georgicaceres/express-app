8fce9ad5c7d3adc69a19bcfbedf364aadf58e4a9
Georgina Cáceres <georgimeli@gmail.com>
2018-06-20 14:15:20 -0300

    code optimized

--- public/javascripts/pagination.js
+++ public/javascripts/pagination.js
@@ -1,32 +1,26 @@
-
+function changePage(page) {
+  let breed = escape($('#selectBreed').val() || '');
+  let size = $('#selectSize').val() || '';
+  let age = $('#selectAge').val()|| '';
+  $('.wrapper.row2').load(`/filter?breed=${breed}&size=${size}&age=${age}&page=${page} #container`);
+}
 
 $(document).ready(function() {
   // Event on specific page
-  $(document).on('click',  '.page', function(event) {
+  $(document).on('click', '.page', function(event) {
     event.preventDefault();
-    let breed = escape($('#selectBreed').val() || '');
-    let size = $('#selectSize').val() || '';
-    let age = $('#selectAge').val()|| '';
     let page = $(this).attr('id');
-    $('.wrapper.row2').load(`/filter?breed=${breed}&size=${size}&age=${age}&page=${page} #container`);
+    changePage(page);
   });
   // Event on Previous button
-  $(document).on('click',  '.prev.enable', function(event) {
+  $(document).on('click', '.prev.enable', function(event) {
     event.preventDefault();
-    let breed = escape($('#selectBreed').val() || '');
-    let size = $('#selectSize').val() || '';
-    let age = $('#selectAge').val() || '';
-    let page = $('.current > a > strong').text();
-    page--;
-    $('.wrapper.row2').load(`/filter?breed=${breed}&size=${size}&age=${age}&page=${page} #container`);
+    let page = parseInt($('.current > a > strong').text()) - 1;
+    changePage(page);
   });
-  $(document).on('click',  '.next.enable', function(event) {
+  $(document).on('click', '.next.enable', function(event) {
     event.preventDefault();
-    let breed = escape($('#selectBreed').val() || '');
-    let size = $('#selectSize').val() || '';
-    let age = $('#selectAge').val() || '';
-    let page = $('.current > a > strong').text();
-    page ++;
-    $('.wrapper.row2').load(`/filter?breed=${breed}&size=${size}&age=${age}&page=${page} #container`);
+    let page = parseInt($('.current > a > strong').text()) + 1;
+    changePage(page);
   });
 })
